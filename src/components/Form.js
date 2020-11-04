cbe1a923db7e4eaaf3e22d5d3635b350ab705e00
Leire Ordeñana <leireomadina@gmail.com>
2020-11-04 13:55:14 +0100

    fix classname

--- src/components/Form.js
+++ src/components/Form.js
@@ -30,93 +30,93 @@ class Form extends React.Component {
           </div>
         </section>
         {/* Fill */}
-        <section class="fill">
-          <div class="design__title js-form-title1" id="2">
-            <div class="design__title-container" id="2">
-              <i class="far fa-keyboard icon-design" id="2"></i>
-              <h2 class="design__main-title" id="2">
+        <section className="fill">
+          <div className="design__title js-form-title1" id="2">
+            <div className="design__title-container" id="2">
+              <i className="far fa-keyboard icon-design" id="2"></i>
+              <h2 className="design__main-title" id="2">
                 Rellena
               </h2>
             </div>
-            <div class="design__down-up js-design__down-up" id="2">
-              <i class="far fa-gem icon-up js-arrow-up" id="2"></i>
+            <div className="design__down-up js-design__down-up" id="2">
+              <i className="far fa-gem icon-up js-arrow-up" id="2"></i>
             </div>
           </div>
-          <form class="fill__form js-form-fill js-form" action="GET">
-            <label for="name" class="label fill__name">
+          <form className="fill__form js-form-fill js-form" action="GET">
+            <label for="name" className="label fill__name">
               Nombre completo
             </label>
             <input
               id="name"
-              class="fill__input js-fill"
+              className="fill__input js-fill"
               type="text"
               name="name"
               placeholder="Ej: Marilyn Monroe"
             />
-            <label for="job" class="label fill__job">
+            <label for="job" className="label fill__job">
               Puesto
             </label>
             <input
-              class="fill__input js-fill"
+              className="fill__input js-fill"
               type="text"
               id="job"
               name="job"
               placeholder="Ej: Diva Developer"
             />
-            <label class="label fill__img" for="text">
+            <label className="label fill__img" for="text">
               Imagen de perfil
             </label>
 
-            <div class="action">
-              <button class="action__upload-btn js__profile-trigger" type="button">
+            <div className="action">
+              <button className="action__upload-btn js__profile-trigger" type="button">
                 Añadir imagen
               </button>
               <input
                 type="file"
                 name=""
                 id="img-selector"
-                class="action__hiddenField js__profile-upload-btn"
+                className="action__hiddenField js__profile-upload-btn"
               />
-              <div class="profile">
-                <div class="profile__preview js__profile-preview"></div>
+              <div className="profile">
+                <div className="profile__preview js__profile-preview"></div>
               </div>
             </div>
-            <label for="email" class="label fill__email">
+            <label for="email" className="label fill__email">
               Email
             </label>
             <input
-              class="fill__input js-fill"
+              className="fill__input js-fill"
               type="email"
               id="email"
               name="email"
               placeholder="Ej: marilyn-monroe@gmail.com"
             />
-            <label for="phone" class="label fill__tel">
+            <label for="phone" className="label fill__tel">
               Teléfono
             </label>
             <input
               id="phone"
-              class="fill__input js-fill"
+              className="fill__input js-fill"
               type="tel"
               name="phone"
               placeholder="Ej: 555-55-55-55"
             />
 
-            <label for="linkedin" class="label fill__linkedin">
+            <label for="linkedin" className="label fill__linkedin">
               Linkedin
             </label>
             <input
-              class="fill__input js-fill"
+              className="fill__input js-fill"
               type="text"
               id="linkedin"
               name="linkedin"
               placeholder="Ej: hollywood.monroe"
             />
-            <label for="github" class="label fill__github">
+            <label for="github" className="label fill__github">
               Github
             </label>
             <input
-              class="fill__input js-fill"
+              className="fill__input js-fill"
               type="text"
               id="github"
               name="github"
@@ -126,37 +126,37 @@ class Form extends React.Component {
         </section>
 
         {/* Share */}
-        <section class="share">
-          <div class="design__title js-form-title1" id="3">
-            <div class="design__title-container" id="3">
-              <i class="fas fa-share-alt icon-design" id="3"></i>
-              <h2 class="design__main-title" id="3">
+        <section className="share">
+          <div className="design__title js-form-title1" id="3">
+            <div className="design__title-container" id="3">
+              <i className="fas fa-share-alt icon-design" id="3"></i>
+              <h2 className="design__main-title" id="3">
                 Comparte
               </h2>
             </div>
-            <div class="design__down-up" id="3">
-              <i class="far fa-gem icon-up js-arrow-up" id="3"></i>
+            <div className="design__down-up" id="3">
+              <i className="far fa-gem icon-up js-arrow-up" id="3"></i>
             </div>
           </div>
-          <div class="js-form-card js-form">
-            <form class="create-card hover js-button-created">
-              <i class="far fa-address-card created-card__button-icon"></i>
+          <div className="js-form-card js-form">
+            <form className="create-card hover js-button-created">
+              <i className="far fa-address-card created-card__button-icon"></i>
               <input
                 type="button"
-                class="create-card__button button__created-title"
+                className="create-card__button button__created-title"
                 value="Crear tarjeta"
               />
             </form>
           </div>
         </section>
         {/* Twitter */}
-        <div class="card js-card card__hidden">
-          <div class="card__created js-form-created">
-            <h2 class="card__created-title">La tarjeta ha sido creada:</h2>
-            <p class="card__created-link js-card-link"></p>
-            <a target="_blank" class="button__twitter js-button-twitter">
-              <i class="fab fa-twitter button__twitter-icon"></i>
-              <h3 class="button__twitter-title">Compartir en twitter</h3>
+        <div className="card js-card card__hidden">
+          <div className="card__created js-form-created">
+            <h2 className="card__created-title">La tarjeta ha sido creada:</h2>
+            <p className="card__created-link js-card-link"></p>
+            <a target="_blank" className="button__twitter js-button-twitter">
+              <i className="fab fa-twitter button__twitter-icon"></i>
+              <h3 className="button__twitter-title">Compartir en twitter</h3>
             </a>
           </div>
         </div>
